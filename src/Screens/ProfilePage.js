efea1142d1ab2b81a20d9da1a770040c0fde49f6
kreisler57 <kreisler57@gmail.com>
2021-07-28 19:53:45 +0700

    nuker

--- src/Screens/ProfilePage.js
+++ src/Screens/ProfilePage.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import {View, Text} from 'react-native';
 
 const ProfilePage = () => {
-    return (
-        <View>
-            <Text></Text>
-        </View>
-    )
-}
+  return (
+    <View>
+      <Text>haee</Text>
+    </View>
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
