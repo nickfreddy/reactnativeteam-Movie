65203f61a8961a146c537c5cfeeac38e07c8ca69
KRyanPutrama <kiagoesryan@gmail.com>
2021-07-29 11:57:16 +0700

    added redux saga, update genre state, added icon on main navigator

---
+++ src/redux/reducer/Movie.js
@@ -0,0 +1,12 @@
+const initialState =  {
+    movieData = []
+}
+
+const movie = (state = initialState, action) => {
+    switch(action.type) {
+        default :
+            return state
+    }
+}
+
+export default movie
