4c4440486034b004da4b1ee9c2f1d90be67075b7
kreisler57 <kreisler57@gmail.com>
2021-08-03 12:12:11 +0700

    image picker adjust

--- src/components/ProfilePic.js
+++ src/components/ProfilePic.js
@@ -6,15 +6,20 @@ const ProfilePic = () => {
   const [image, setImage] = useState(
     'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png',
   );
-  const ChooseFromGallery = () => {
-    ImagePicker.openPicker({
+  const ChooseFromGallery = async () => {
+    await ImagePicker.openPicker({
       width: 300,
       height: 400,
-      cropping: true,
-    }).then(image => {
-      console.log(image);
-      setImage(image.path);
-    });
+      cropping: false,
+      mediaType: 'photo',
+    })
+      .then(image => {
+        console.log(image);
+        setImage(image.path);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
   return (
